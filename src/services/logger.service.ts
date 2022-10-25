65fa6e4fc94189acaca9c7a68fd2c46aa4b0f895
BarBronshtein <barbronshtein@gmail.com>
2022-10-25 17:17:58 +0300

    added logger service

---
+++ src/services/logger.service.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+
+const logsDir = './logs';
+if (!fs.existsSync(logsDir)) fs.mkdirSync(logsDir);
+
+// Define the time format
+function getTime() {
+	return new Date().toLocaleString();
+}
+
+function doLog(level: string, ...args: unknown[]) {
+	const strs = args.map(arg =>
+		typeof arg === 'string' ? arg : JSON.stringify(arg)
+	);
+	let line = strs.join(' | ');
+	line = `${getTime()} - ${level} - ${line}\n`;
+	console.log(line);
+	fs.appendFileSync('./logs/backend.log', line);
+}
+
+module.exports = {
+	debug(...args: unknown[]) {
+		// if (process.env.NODE_NEV === 'production') return
+		doLog('DEBUG', ...args);
+	},
+	info(...args: unknown[]) {
+		doLog('INFO', ...args);
+	},
+	warn(...args: unknown[]) {
+		doLog('WARN', ...args);
+	},
+	error(...args: unknown[]) {
+		doLog('ERROR', ...args);
+	},
+};
