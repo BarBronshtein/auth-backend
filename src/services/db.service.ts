924f24e76536c51c0acaa1e47e07f2d80c684f5d
BarBronshtein <barbronshtein@gmail.com>
2022-10-25 17:43:20 +0300

    added db service

---
+++ src/services/db.service.ts
@@ -0,0 +1,37 @@
+import { MongoClient, MongoClientOptions, Db } from 'mongodb';
+
+import config from './config';
+import logger from './logger-service';
+
+module.exports = {
+	getCollection,
+};
+
+// Database name
+const dbName = 'auth_db';
+
+let dbConn: Db | null = null;
+
+async function getCollection(collectionName: string) {
+	try {
+		const db = await connect();
+		const collection = await db.collection(collectionName);
+		return collection;
+	} catch (err) {
+		logger.error('Faile to get Mongo collection', err);
+		throw err;
+	}
+}
+
+async function connect() {
+	if (dbConn) return dbConn;
+	try {
+		const client = new MongoClient(config.dbURL);
+		const db = client.db(dbName);
+		dbConn = db;
+		return db;
+	} catch (err) {
+		logger.error('Cannot Connect to DB', err);
+		throw err;
+	}
+}
