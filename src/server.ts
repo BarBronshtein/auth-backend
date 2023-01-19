3be288ad95ded5287d2e741d6fa78d73170b8c69
BarBronshtein <barbronshtein@gmail.com>
2023-01-19 14:26:33 +0200

    added another origin to cors

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@ app.use(express.json());
 
 const corsOptions = {
 	origin: [
+		'http://127.0.0.1:80',
+		'http://localhost:80',
 		'http://127.0.0.1:8080',
 		'http://localhost:8080',
 		'http://127.0.0.1:3000',
